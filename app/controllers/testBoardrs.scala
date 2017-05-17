bfd7bdc038e446fff57384e039ee40fd79c85dcb
sambodhinio <sambodhinio@hotmail.com>
2017-05-17 21:13:07 +0100

    4th change 17/5

--- app/controllers/testBoardrs.scala
+++ app/controllers/testBoardrs.scala
@@ -3,18 +3,50 @@ package controllers
 /**
   * Created by sambo on 10/05/2017.
   */
+
+// TODO - determine database
+case class ResortStatic(id: Int, name: String, countryCode: String, country: String, latLongLst: List[Double], areaSize_km2: Double, nearestIata: String)
+//case class Resort(surname: String, static: Map[String, ResortStatic])
+case class Resort(surname: String, static: ResortStatic)
+
+//case class Resort[A](sMap: Map[String, ResortStatic])
+//case class Person(name: String, age: Int)
+
 object testBoardrs {
 
   def main(args: Array[String]): Unit = {
 
-    val uLoc = Array(51.468125, -0.127886)
-    val rLoc = Array(45.955767, 6.885117)
-    val dist = getDistance(haversineMethod, uLoc, rLoc)
-    println(roundSmart(dist))
+    //val person1 = Person("Samuel", 36)
+    val resortStatic1 = ResortStatic(1, "Chamonix", "FRA", "France", List(50.114929, -122.948626), 18.2, "GVA")
+
+    val resort1 = Resort("Brooks", resortStatic1)
+    println(resort1.static.latLongLst(1))
+    //val resort1 = Resort(sMap)
+
+    /*
+
+    //val resort2 = Resort(map1("id" => "2", "name" => "Chamonix"))
+
+    val list1 = List(50.114929, -122.948626)
+    //val map1 = list1.map(c => c * 2)
+    val r1 = resort1.sMap("rLatLongLst")
+
+    val list2 = List(1, 2, List("a", "b", "c"))
+    //val list3 = List("a", "b", "c")
+    println(list2(2).get(0))
+    */
+    //printDistance
     //println(roundSmart(7.9121))
   }
 
-//  def testCollections1: Boolean = Vector(1, 2, 3) == Range(1, 2, 3)
+  def printDistance: Unit = {
+    val uLoc = Array(51.468125, -0.127886)
+    val rLoc = Array(45.955767, 6.885117)
+    val dist = getDistance(haversineMethod, uLoc, rLoc)
+
+    println(roundSmart(dist))
+  }
+
   //def mapToList[a, b](m: Map[a, b]): List[(a, b)] = m.toList
 
   def rad(x: Double):Double = x * (scala.math.Pi / 180)
