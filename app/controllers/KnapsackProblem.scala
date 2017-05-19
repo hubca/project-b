e85480ac1e00f55d827f6a926aa0686ba8995a91
hubca <sambodhinio@hotmail.com>
2017-05-19 22:29:54 +0100

    renamed project so should work for paynod. Added TestController to enable testing of MVC structure

--- app/controllers/KnapsackProblem.scala
+++ app/controllers/KnapsackProblem.scala
@@ -10,14 +10,19 @@ object KnapsackProblem {
     val smallArr = Array(159, 162, 224, 128, 162, 3, 217, 208)
     val mediumArr = Array(156, 108, 125, 58, 211, 162, 163, 52, 208, 181, 205, 68, 297, 131, 162, 197, 111, 166, 55, 131, 440, 138, 349, 165, 205)
     val bigArr = Array(156, 108, 125, 58, 211, 162, 163, 52, 208, 181, 205, 68, 297, 131, 162, 197, 111, 166, 55, 131, 440, 138, 349, 165, 205, 156, 157, 162, 159, 342, 58, 414, 370, 178, 505, 111, 327, 330, 163, 156, 168, 168, 194, 153, 54, 238, 110, 127, 254, 270, 293, 165, 58, 107, 222, 239, 54, 208, 162, 199, 159, 158, 165, 156, 168, 217)
-    val limit = 4500
+    val limit = 3213
+
+    val mediumLst = List(55, 85, 131, 440, 39, 138, 349, 165, 205, 95, 156, 157, 162, 159, 342, 58, 414, 370, 178, 505, 111, 327, 330, 163, 156, 168, 168, 194, 153, 54, 238, 110, 127, 254, 270, 293, 165, 58, 51, 107, 222, 239, 52, 54, 208, 162, 199, 159, 158, 165, 156, 168, 217)
+    val mediumStr = Stream(156, 108, 125, 58, 211, 162, 163, 52, 208, 181, 205, 68, 297, 131, 162, 197, 111, 166, 55, 131, 440, 138, 349, 165, 205)
 
     try {
       val arr = mediumArr
       //executeTimePrint(method1(arr, limit).toList)
       //executeTimePrint(method2(arr, limit).toList)
       //executeTimePrint(method3(arr, limit).toList)
-      executeTimePrint(method4(arr, limit).toList)
+      //executeTimePrint(method4(arr, limit).toList)
+      executeTimePrint(method5(mediumLst, limit))
+      //executeTimePrint(method6(mediumStr, limit).toList)
     } catch {
       case ex: UnsupportedOperationException => println("Limit cannot be matched exactly so doesn't work with method 1")
       case _: Throwable => println("Got some other kind of exception")
@@ -65,7 +70,35 @@ object KnapsackProblem {
       case _ => val res = (1 to subset.length).view
         .flatMap(subset.combinations)
         .find(_.sum == limit)
-        if (res.isEmpty) method3(subset, limit-1)
+        if (res.isEmpty) method4(subset, limit-1)
+        else res.get
+    }
+  }
+
+  // tail recursive solution with pattern matching (update to method4) but using list
+  def method5(lst: List[Int], limit: Int): List[Int] = {
+    val subset = lst.filter(limit.>=)
+
+    subset match {
+      case subset if subset.sum <= limit => subset
+      case _ => val res = (1 to subset.length).view
+        .flatMap(subset.combinations)
+        .find(_.sum == limit)
+        if (res.isEmpty) method5(subset, limit-1)
+        else res.get
+    }
+  }
+
+  // tail recursive solution with pattern matching (update to method4) but using stream
+  def method6(str: Stream[Int], limit: Int): Stream[Int] = {
+    val subset = str.filter(limit.>=)
+
+    subset match {
+      case subset if subset.sum <= limit => subset
+      case _ => val res = (1 to subset.length).view
+        .flatMap(subset.combinations)
+        .find(_.sum == limit)
+        if (res.isEmpty) method6(subset, limit-1)
         else res.get
     }
   }
