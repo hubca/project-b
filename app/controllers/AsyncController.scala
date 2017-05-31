6446ea78cedccbe3451f2dc5074363dcc9aa370d
hubca <sambodhinio@hotmail.com>
2017-05-31 20:04:05 +0100

    renamed project so should work for paynod. Added TestController to enable testing of MVC structure

--- app/controllers/AsyncController.scala
+++ app/controllers/AsyncController.scala
@@ -2,40 +2,22 @@ package controllers
 
 import akka.actor.ActorSystem
 import javax.inject._
-import play.api._
 import play.api.mvc._
-import scala.concurrent.{ExecutionContext, Future, Promise}
-import scala.concurrent.duration._
+import services.ServiceClient
+import scala.concurrent.ExecutionContext
 
-/**
- * This controller creates an `Action` that demonstrates how to write
- * simple asynchronous code in a controller. It uses a timer to
- * asynchronously delay sending a response for 1 second.
- *
- * @param actorSystem We need the `ActorSystem`'s `Scheduler` to
- * run code after a delay.
- * @param exec We need an `ExecutionContext` to execute our
- * asynchronous code.
- */
 @Singleton
-class AsyncController @Inject() (actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
+class AsyncController @Inject() (sc: ServiceClient)(actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
 
-  /**
-   * Create an Action that returns a plain text message after a delay
-   * of 1 second.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/message`.
-   */
-  def message = Action.async {
-    getFutureMessage(1.second).map { msg => Ok(msg) }
-  }
+  def index = Action.async { request =>
 
-  private def getFutureMessage(delayTime: FiniteDuration): Future[String] = {
-    val promise: Promise[String] = Promise[String]()
-    actorSystem.scheduler.scheduleOnce(delayTime) { promise.success("Hi!") }
-    promise.future
+    val asy1 = sc.makeServiceCall("async1")
+
+    for {
+      async1Message <- asy1
+    } yield {
+      Ok(views.html.async1.async1(async1Message))
+    }
   }
 
 }
